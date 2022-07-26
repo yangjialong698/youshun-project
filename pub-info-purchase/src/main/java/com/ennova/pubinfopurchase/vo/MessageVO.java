ce12d5ccbb95d1960ce3fed9f66a0ee7115ee86c
yuhaishan <2537650609@qq.com>
2022-07-26 16:02:12 +0800

    采购信息-供应商认证-供应商列表支持id搜索

---
+++ pub-info-purchase/src/main/java/com/ennova/pubinfopurchase/vo/MessageVO.java
@@ -0,0 +1,19 @@
+package com.ennova.pubinfopurchase.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class MessageVO<T> {
+    //来源：0 公告 1 供应商认证  2 日报  3 经验建议
+    private int sourceType;
+
+    //类型 0 - 新增  1- 审核通过 2-审核不通过  3-修改  4.日报反馈  5.经验建议评论
+    private int type;
+
+    //内容
+    private T content;
+
+    private Integer userId;
+}
