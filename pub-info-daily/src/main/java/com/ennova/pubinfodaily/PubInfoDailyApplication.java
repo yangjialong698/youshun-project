03932041636f0b9c1bb691fd89437bcde7f6ee15
汪炜 <646982196@qq.com>
2022-08-11 13:55:16 +0800

    新建日报模块

---
+++ pub-info-daily/src/main/java/com/ennova/pubinfodaily/PubInfoDailyApplication.java
@@ -0,0 +1,13 @@
+package com.ennova.pubinfodaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubInfoDailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubInfoDailyApplication.class, args);
+    }
+
+}
