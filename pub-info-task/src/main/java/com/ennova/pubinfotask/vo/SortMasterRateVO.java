762c5b95898720c2c5dbcbb0e55b19488f68f815
shibingyang1990 <shibingyang1990@gmail.com>
2022-09-15 16:07:46 +0800

    新增负责人和截止日期

--- pub-info-task/src/main/java/com/ennova/pubinfotask/vo/SortMasterRateVO.java
+++ pub-info-task/src/main/java/com/ennova/pubinfotask/vo/SortMasterRateVO.java
@@ -1,6 +1,9 @@
 package com.ennova.pubinfotask.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class SortMasterRateVO {
@@ -9,7 +12,12 @@ public class SortMasterRateVO {
 
     private String name;
 
+    private String username;
+
     private Integer rate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date taskEndDate;
 
     private Integer sortNum;
 
