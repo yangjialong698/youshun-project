43c3580990b53f7f9c15631d68e74f6a58ee9747
yangjialong <2537650609@qq.com>
2022-09-19 14:33:02 +0800

    官网模块module

---
+++ pub-info-website/src/main/java/com/ennova/pubinfowebsite/PubInfoWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ennova.pubinfowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubInfoWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubInfoWebsiteApplication.class, args);
+    }
+
+}
