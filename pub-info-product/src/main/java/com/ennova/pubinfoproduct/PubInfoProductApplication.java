6042133349655f8e2e12d702bc250d94ef7dbbc0
wangwei <646982196@qq.com>
2022-10-11 08:59:17 +0800

    总部产品模块搭建第一版

---
+++ pub-info-product/src/main/java/com/ennova/pubinfoproduct/PubInfoProductApplication.java
@@ -0,0 +1,13 @@
+package com.ennova.pubinfoproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubInfoProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubInfoProductApplication.class, args);
+    }
+
+}
