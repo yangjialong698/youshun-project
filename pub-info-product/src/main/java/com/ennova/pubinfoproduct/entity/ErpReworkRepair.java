3f8478932a53bf83912da7a701caa78961502ce6
汪炜 <646982196@qq.com>
2023-02-09 13:45:18 +0800

    返工返修及客诉新增供应商表+入库供应商代号名称+总数BUG修复

--- pub-info-product/src/main/java/com/ennova/pubinfoproduct/entity/ErpReworkRepair.java
+++ pub-info-product/src/main/java/com/ennova/pubinfoproduct/entity/ErpReworkRepair.java
@@ -1,6 +1,8 @@
 package com.ennova.pubinfoproduct.entity;
 
 import java.util.Date;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,50 +24,60 @@ public class ErpReworkRepair {
     /**
      * 反馈人id
      */
+    @ApiModelProperty(value = "反馈人id")
     private Integer backUserId;
 
     /**
      * 供应商编号
      */
+    @ApiModelProperty(value = "供应商编号")
     private Integer supplierNo;
 
     /**
      * 供应商名称
      */
+    @ApiModelProperty(value = "供应商名称")
     private String supplier;
 
     /**
      * 品号
      */
+    @ApiModelProperty(value = "品号")
     private String productNo;
 
     /**
      * 品名
      */
+    @ApiModelProperty(value = "品名")
     private String productName;
 
     /**
      * 返修数量
      */
+    @ApiModelProperty(value = "返修数量")
     private Integer num;
 
     /**
      * 总数量
      */
+    @ApiModelProperty(value = "总数量")
     private Integer totalNum;
 
     /**
      * 返工原因
      */
+    @ApiModelProperty(value = "返工原因")
     private String reworkReason;
 
     /**
      * 返工工时
      */
+    @ApiModelProperty(value = "返工工时")
     private Integer reworkHour;
 
     /**
      * 返工日期
      */
+    @ApiModelProperty(value = "返工日期")
     private Date reworkTime;
 }
